9c74bf245c6b0f607ed932c4d8791830527bda97
SmartDragon <smartdragon2462@gmail.com>
2021-07-16 19:43:42 +0200

    Updated search function.

--- src/routes/home/index.jsx
+++ src/routes/home/index.jsx
@@ -2,28 +2,34 @@ import React, { useState, useEffect } from "react";
 import MapBox from "../../components/mapBox";
 import axios from "axios";
 import { REST_COUNTRIES_ENDPOINT } from "../../config/api";
+import _ from "lodash";
 
 export default function Home() {
   const [searchTerm, setSearchTerm] = useState("");
   const [coordinates, setCoordinates] = useState([]);
   const [countryList, setCountryList] = useState([]);
-  const [availableList, setAvailableList] = useState([]);
+  const [show, setShow] = useState(false);
 
   useEffect(() => {
-    if (searchTerm)
+    if (searchTerm) {
       axios
         .get(`${REST_COUNTRIES_ENDPOINT}/name/${searchTerm}`)
         .then(({ data }) => {
-          setAvailableList([...data]);
+          console.log("data : ", data);
+          setShow(true);
+          setCountryList([...data]);
+        })
+        .catch((err) => {
+          setShow(false);
         });
+    } else {
+      setShow(false);
+    }
   }, [searchTerm]);
-
-  const onHandleClickSearch = () => {
-    axios
-      .get(`${REST_COUNTRIES_ENDPOINT}/name/${searchTerm}`)
-      .then(({ data }) => {
-        setCountryList([...data]);
-      });
+  const onHandleSelection = (country) => {
+    setSearchTerm("");
+    setShow(false);
+    setCountryList([country]);
   };
   return (
     <div>
@@ -32,16 +38,15 @@ export default function Home() {
           value={searchTerm}
           onChange={(ev) => setSearchTerm(ev.target.value)}
         />
-        <button onClick={onHandleClickSearch}>Search</button>
-        {availableList.length > 0 && (
+        {countryList.length > 0 && show ? (
           <ul className="auto-complete-list">
-            {availableList.map((country, index) => (
-              <li key={index} onClick={() => setCountryList([country])}>
+            {countryList.map((country, index) => (
+              <li key={index} onClick={() => onHandleSelection(country)}>
                 {country.name}
               </li>
             ))}
           </ul>
-        )}
+        ) : null}
       </div>
       <MapBox coordinates={coordinates} />
       <ul>
